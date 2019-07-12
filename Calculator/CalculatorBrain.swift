3295c810cd3a0ef46104189b631b7e808d66904f
Jake Connerly <jlconnerly@gmail.com>
2019-07-12 10:11:14 -0500

    Initial Commit

---
+++ Calculator/CalculatorBrain.swift
@@ -0,0 +1,31 @@
+//
+//  CalculatorBrain.swift
+//  Calculator
+//
+//  Created by Ben Gohlke on 5/30/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+
+enum OperatorType: String {
+    case addition = "+"
+}
+
+class CalculatorBrain {
+    var operand1String = ""
+    var operand2String = ""
+    var operatorType: OperatorType?
+    
+    func addOperandDigit(_ digit: String) -> String {
+        
+    }
+    
+    func setOperator(_ operatorString: String) {
+        
+    }
+    
+    func calculateIfPossible() -> String? {
+        
+    }
+}
